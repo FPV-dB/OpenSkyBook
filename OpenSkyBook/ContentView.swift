4357a26c8235169e2962553e31dd8a322deb3853
db <dedbundyy@icloud.com>
2026-03-29 02:06:21 +1030

    Initial Commit

---
+++ OpenSkyBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenSkyBook
+//
+//  Created by M E on 29/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenSkyBook/OpenSkyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSkyBookApp.swift
+//  OpenSkyBook
+//
+//  Created by M E on 29/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenSkyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
